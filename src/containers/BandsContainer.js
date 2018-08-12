e111fcb82edf8934752821021435e980bc38725c
Randall Robinson <keith_adams007@yahoo.com>
2018-08-12 23:19:59 +0000

    automatically backed up by learn

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -7,7 +7,7 @@ import BandInput from './BandInput'
 class BandsContainer extends Component {
 
   renderBands = () => {
-    this.props.bands.map((band, id) => <BandInput key={id} text={band})
+    this.props.bands.map((band, id) => <BandInput key={id} text={band} />)
   }
 
   render() {
