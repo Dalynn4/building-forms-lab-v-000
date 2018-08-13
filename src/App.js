3ddc741ef940115b02616801a9f3dd8a6c65bb4f
Randall Robinson <keith_adams007@yahoo.com>
2018-08-13 01:36:47 +0000

    automatically backed up by learn

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <BandsContainer store={this.props.store}/>
+        <BandsContainer />
       </div>
     );
   }
